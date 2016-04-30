317bcac8a4c5ea1d3b689753c68e9a8f22cbad76
Jaryn Seidl <jarynseidl@gmail.com>
2016-04-30 10:04:40 -0600

    Move based on relative location to goal

--- src/apriltags_intrude_detector/scripts/sphero_intrude_gui.py
+++ src/apriltags_intrude_detector/scripts/sphero_intrude_gui.py
@@ -24,7 +24,7 @@ class Controller:
             # Change twist.linear.x to be your desired x velocity
             twist.linear.x += self.fields[0].calcVelocity(msg)[0]
             # Change twist.linear.y to be your desired y velocity
-            twist.linear.y += self.fiels[0].calcVelocity(msg)[1]
+            twist.linear.y += self.fields[0].calcVelocity(msg)[1]
             twist.linear.z = 0
             twist.angular.x = 0
             twist.angular.y = 0

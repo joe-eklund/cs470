c494dacdfc9cb76f65c9488ecf9c5131b485a590
jarynseidl <jarynseidl@gmail.com>
2016-05-10 15:52:02 -0600

    RRT getNearest and Vertex.distance

--- src/apriltags_intrude_detector/scripts/RRT.py
+++ src/apriltags_intrude_detector/scripts/RRT.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python
 
 import random
+import math
+
 class Tree:
 
     def __init__(self,vertex):
         self.vertices = []
         self.edges = []
         self.vertices.append(vertex)
+    def create(self):
+        self
     def addVertex(self, vertex):
         self.vertices.append(vertex)
     def addEdge(self, edge):
@@ -14,11 +18,19 @@ class Tree:
     def generateVertex(self):
         return Vertex(random.randrange(0,800,1),random.randrange(0,600,1))
     def getNearest(self,new_vertex):
+        nearestVertex = None
+        distance = 10000
         for vertex in self.vertices:
-            vertex
+            if new_vertex.distance(vertex) < distance:
+                distance = new_vertex.distance(vertex)
+                nearestVertex = vertex
+        return nearestVertex
 
 
 class Vertex:
     def __init__(self,x,y):
         self.x = x
         self.y = y
+
+    def distance(self,vertex):
+        return math.sqrt(math.pow(vertex.x - self.x,2) + math.pow(vertex.y - self.y,2))
